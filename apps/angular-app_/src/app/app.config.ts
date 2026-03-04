c492fe1607fa55646a6e52a05b3416a5f215abba
David Pestana Perdomo <david.pestana.perdomo@gmail.com>
2026-03-04 12:33:40 +0000

    api

--- apps/angular-app_/src/app/app.config.ts
+++ apps/angular-app_/src/app/app.config.ts
@@ -10,7 +10,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideKeycloak({
       config: {
-        url: 'https://congenial-acorn-rx7v9q5w4wcw7vx-8080.app.github.dev', 
+        url: 'https://congenial-acorn-rx7v9q5w4wcw7vx-8080.app.github.dev/', 
         realm: 'training',
         clientId: 'spa-client'
        },
